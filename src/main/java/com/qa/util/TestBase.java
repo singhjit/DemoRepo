273b078d9fdb16b28da88bc674c7a9a3eb8b1333
Jit <jeetu.dec25@gmail.com>
2022-09-26 07:17:44 -0400

    testing

--- src/main/java/com/qa/util/TestBase.java
+++ src/main/java/com/qa/util/TestBase.java
@@ -5,5 +5,9 @@ public class TestBase {
 	public void test() {
 		System.out.println("test method");
 	}
+	
+	public void test1() {
+		System.out.println("test1 method");
+	}
 
 }
---
+++ src/test/java/com/qa/profile/Profile.java
@@ -0,0 +1,9 @@
+package com.qa.profile;
+
+public class Profile {
+	
+	public void profile() {
+		System.out.println("Profile");
+	}
+
+}
