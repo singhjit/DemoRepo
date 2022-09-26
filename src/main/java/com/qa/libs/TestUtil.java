621f3501cafcc8a8b17e9a9cd280065e09d21db6
Jit <jeetu.dec25@gmail.com>
2022-09-26 05:51:52 -0400

    first commit

---
+++ src/main/java/com/qa/libs/TestUtil.java
@@ -0,0 +1,9 @@
+package com.qa.libs;
+
+public class TestUtil {
+	
+	public void testUtil() {
+		System.out.println("Test Util");
+	}
+
+}
---
+++ src/main/java/com/qa/util/TestBase.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class TestBase {
+	
+	public void test() {
+		System.out.println("test method");
+	}
+
+}
