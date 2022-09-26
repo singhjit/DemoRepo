05b4e3f3922d46aa720bcd4c531a7c9e4f684023
Jit <jeetu.dec25@gmail.com>
2022-09-26 07:42:17 -0400

    added chnages as per review comment

--- src/test/java/com/qa/profile/Profile.java
+++ src/test/java/com/qa/profile/Profile.java
@@ -3,7 +3,7 @@ package com.qa.profile;
 public class Profile {
 	
 	public void profile() {
-		System.out.println("Profile");
+		System.out.println("Profile1");
 		System.out.println("Profile2");
 
 	}
